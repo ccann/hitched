8d12c035a6634c8db0e4e45f6b068b0c0a05efcc
Cody Canning <cocanning11@gmail.com>
2017-12-24 13:54:04 -0500

    remove other stuff

--- dev/cljs/user.cljs
+++
@@ -1,8 +0,0 @@
-(ns cljs.user
-  (:require [hitched.core]
-            [hitched.system :as system]))
-
-(def go system/go)
-(def reset system/reset)
-(def stop system/stop)
-(def start system/start)
--- src/cljc/hitched/common.cljc
+++
@@ -1,7 +0,0 @@
-(ns hitched.common)
-
-(defn shared-fn
-  "A function that is shared between clj and cljs"
-  []
-  (println "cljc!"))
-
